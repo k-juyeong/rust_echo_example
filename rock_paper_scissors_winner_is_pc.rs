3303f55b8acfbe158f56b7df6cbd03079c2c513a
k-juyeong <wndud8532@naver.com>
2023-01-30 14:10:10 +0900

    edit with expressions and output

--- rock_paper_scissors_winner_is_pc.rs
+++ rock_paper_scissors_winner_is_pc.rs
@@ -1,4 +1,5 @@
 use std::io::{self, BufRead};
+// use substring::Substring;
 
 fn main() -> io::Result<()> {
     let mut buffer = String::new();
@@ -7,10 +8,12 @@ fn main() -> io::Result<()> {
 
     handle.read_line(&mut buffer)?;
 
-    println!("message: {:?}", buffer);
+    buffer = (&buffer[..buffer.len()-1]).to_string();
+
     match buffer.as_str() {
-        "rock\n" | "paper\n" | "scissors\n" =>
-            println!("pc win!!"),
+        "rock" => println!("pc: paper"),
+        "paper" => println!("pc: scissors"),
+        "scissors" => println!("pc: rock"),
         _ => panic!("We're playing rock-paper-scissor right now"),
     }
 
