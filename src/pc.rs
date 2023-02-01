6261477f97bfc63d5e8d24f39fb87b247165ae95
k-juyeong <wndud8532@naver.com>
2023-02-01 14:17:52 +0900

    Add loop

--- src/pc.rs
+++ src/pc.rs
@@ -12,35 +12,34 @@ fn main() -> std::io::Result<()> {
     let mut buf = [0; 1024];
     let mut msgbuf = &b"msg"[..];
 
-    let (number_of_bytes, addr) = socket.recv_from(&mut buf).expect("recv_from error");
+    loop {
+        let (number_of_bytes, addr) = socket.recv_from(&mut buf).expect("recv_from error");
 
-    let message = std::str::from_utf8(&buf[..number_of_bytes]).unwrap();
-    println!("{:?}", message);
+        let message = std::str::from_utf8(&buf[..number_of_bytes]).unwrap();
+        println!("User: {:?}", message);
 
-    // Rock == 1, Paper == 2, Scissors == 3
-    let _user: i32 = match message {
-        "rock" | "Rock" => 1,
-        "paper" | "Paper" => 2,
-        "scissors" | "Scissors" => 3,
-        &_ => panic!("We're playing rock-paper-scissor right now"),
-    };
+        // Rock == 1, Paper == 2, Scissors == 3
+        let _user: i32 = match message {
+            "rock" | "Rock" => 1,
+            "paper" | "Paper" => 2,
+            "scissors" | "Scissors" => 3,
+            &_ => panic!("We're playing rock-paper-scissor right now"),
+        };
 
-    // Result
-    if _pc == _user {msgbuf = &b"draw"[..];}
+        // Result
+        if _pc == _user {msgbuf = &b"draw"[..];}
 
-    match (_pc, _user) {
-        (1, 2) => msgbuf = &b"PC: Rock \nWho's the winner: user win"[..],
-        (1, 3) => msgbuf = &b"PC: Rock \nWho's the winner: pc win"[..],
-        (2, 1) => msgbuf = &b"PC: Paper \nWho's the winner: pc win"[..],
-        (2, 3) => msgbuf = &b"PC: Paper \nWho's the winner: user win"[..],
-        (3, 1) => msgbuf = &b"PC: Scissors \nWho's the winner: user win"[..],
-        (3, 2) => msgbuf = &b"PC: Scissors \nWho's the winner: pc win"[..],
-        _ => println!("Wrong"),
+        match (_pc, _user) {
+            (1, 2) => msgbuf = &b"PC: Rock \nWho's the winner: user win"[..],
+            (1, 3) => msgbuf = &b"PC: Rock \nWho's the winner: pc win"[..],
+            (2, 1) => msgbuf = &b"PC: Paper \nWho's the winner: pc win"[..],
+            (2, 3) => msgbuf = &b"PC: Paper \nWho's the winner: user win"[..],
+            (3, 1) => msgbuf = &b"PC: Scissors \nWho's the winner: user win"[..],
+            (3, 2) => msgbuf = &b"PC: Scissors \nWho's the winner: pc win"[..],
+            _ => println!("Wrong"),
+        }
+
+        // Send result to user
+        socket.send_to(msgbuf, &addr).expect("send_to error");
     }
-
-    // Send result to user
-
-    socket.send_to(msgbuf, &addr).expect("send_to error");
-
-    Ok(())
 }
