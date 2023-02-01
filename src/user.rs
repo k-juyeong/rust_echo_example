21941d74c46c0cb8ae439ba20f72d7a330cdfbe4
k-juyeong <wndud8532@naver.com>
2023-02-01 13:38:28 +0900

    Add explanatin for playing

--- src/user.rs
+++ src/user.rs
@@ -4,10 +4,13 @@ use std::io::{self, BufRead};
 fn main() -> std::io::Result<()> {
     let socket = UdpSocket::bind("127.0.0.1:3401").expect("bind error");
 
+    println!("Let's play rock-paper-scissor!! Your opponent is PC ");
+    println!("3, 2, 1 >> ");
+
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = line.unwrap();
-        
+
         match line.as_str() {
             "rock" | "Rock" | "paper" | "Paper" | "scissors" | "Scissors" =>
                 print!("Who's the winner: "),
